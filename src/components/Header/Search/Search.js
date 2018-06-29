abb4c022ea9473772e2884bf88448a4ab79ba78c
Seth Adamson <seth.c.adamson@gmail.com>
2018-06-29 00:18:30 -0600

    login css and initial search setup

--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -13,15 +13,28 @@ class Search extends Component {
     }
 
     render(){
-        let {search, searchToggle, searchFocus} = this.props;
+        let {search, searchToggle, searchFocus, boards, lists, cards} = this.props;
         console.log(this.props, this.state)
+        let boardsFilter = [];
+        let listsFilter = [];
+        let cardsFilter = [];
         if(search){
             searchFocus();
+            boardsFilter = boards.filter(b => b.name === search);
+            listsFilter = lists.filter(l => l.list_title === search);
+            cardsFilter = cards.filter(c => c.card_title === search);
         }
+
+        let boardsDisplay = boardsFilter.map(b => <div key={b.id}>{b.name}</div> );
+        let listsDisplay = listsFilter.map(l => <div key={l.list_id}>{l.list_title}</div>);
+        let cardsDisplay = cardsFilter.map(c => <div key={c.id}>{c.card_title}</div>);
+
         return (
             <div className={searchToggle ? 'search-shown' : 'search-hidden'}>
                 <div className='search-content'>
-
+                    <h4 className='search-title'>Boards: {boardsDisplay}</h4>
+                    <h4 className='search-title'>Lists: {listsDisplay}</h4>
+                    <h4 className='search-title'>Cards: {cardsDisplay}</h4>
                 </div> 
             </div> 
         )
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,29 +1,57 @@
 import React, {Component} from 'react';
 import './Login.css';
 import targetWhite from '../../images/targetWhite.png'
+import loginVideo from '../../media/loginVideo.mp4';
+import woodBG1 from '../../media/woodBG-1.jpg';
 
 export default class Login extends Component {
     constructor(){
         super();
 
         this.state = {
-
+            loginDisplay: false,
         }
+
+        this.showLogin = this.showLogin.bind(this);
+        this.loginInterval = this.loginInterval.bind(this);
+
+    }
+
+    showLogin(){
+        this.setState({loginDisplay: true});
+    }
+
+    loginInterval(){
+        setTimeout(this.showLogin, 4000);
     }
 
     render(){
+        let {loginDisplay} = this.state;
+        this.loginInterval();
         return(
-            <div className='login-parent'>
+            <div className='login-parent' style={{backgroundImage: `url(${woodBG1})`}} onClick={this.showLogin}>
                 <div className='login-content'>
-                    <a className='login-logo'>
-                        <img className='login-img' src={targetWhite} alt='logo' />
-                        <h1 className='login-title'>n-Target</h1>
-                    </a>
-                    <a href={process.env.REACT_APP_LOGIN} >
-                        <button className='login-button'>Login</button>
-                    </a>
+                    <div className='login-video'>
+                        <video className='login-video-player' autoplay='true' loop muted>
+                            <source src={loginVideo} type='video/mp4'/>
+                        </video>
+                    </div>
+                    <div className={loginDisplay ? 'login-menu-shown' : 'login-menu-hidden'}>
+                        <h2 className='login-logo'>
+                            <img className='login-img' src={targetWhite} alt='logo' />
+                            <h1 className='login-title'>n-Target</h1>
+                        </h2>
+                        <a href={process.env.REACT_APP_LOGIN} className='login-button'>
+                            Login
+                        </a>
+                    </div>  
                 </div> 
             </div> 
         )
     }
 }
+
+{/* <h2 className='login-logo'>
+    <img className='login-img' src={targetWhite} alt='logo' />
+    <h1 className='login-title'>n-Target</h1>
+</h2> */}
