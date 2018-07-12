2daf17e6579a2892d13b91263e7f9d1b2d3d368b
Seth Adamson <seth.c.adamson@gmail.com>
2018-07-12 16:16:33 -0600

    mobile display change

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import {HashRouter} from 'react-router-dom';
 import {DragDropContext} from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
+import LogoTarget from './media/LogoTarget.png';
+import woodBG1 from './media/woodBG-1.jpg';
 import CustomDragLayer from './components/Board/List/Card/CardDND/CustomDragLayer';
 
 
@@ -12,7 +14,13 @@ class App extends Component {
     return (
       <HashRouter>
         <div className="App">
+        <div className='nonresponsive' style={{backgroundImage: `url(${woodBG1})`}}>
+          <img className='app-target' src={LogoTarget} alt='logo' />
+          <p className='mobile-title'>is best viewed on larger screens</p>
+        </div> 
+        <div className='routes'>
           {routes}
+        </div> 
           {/* <CustomDragLayer />  */}
         </div>
       </HashRouter>
